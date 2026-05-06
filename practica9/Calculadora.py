aefbc4c6359559d5ebe158c6a2a298081b3402c3
alangabrielalvarez92-dev <alangabrielalvarez92@gmail.com>
2026-05-06 11:46:48 -0600

    Se agregó la potencia y la división entera

--- practica9/Calculadora.py
+++ practica9/Calculadora.py
@@ -8,6 +8,8 @@ print("1. Suma")
 print("2. Resta")
 print("3. Multiplicación")
 print("4. División")
+print("5. Potencia")
+print("6. División entera")
 
 opcion = input("Opción: ")
 
@@ -26,6 +28,17 @@ elif opcion == "3":
 elif opcion == "4":
     if num2 != 0:
         resultado = num1 / num2
+        print(f"Resultado: {resultado}")
+    else:
+        print("Error: No se puede dividir entre cero")
+
+elif opcion == "5":
+    resultado = num1 ** num2
+    print(f"Resultado: {resultado}")
+
+elif opcion == "6":
+    if num2 != 0:
+        resultado = num1 // num2
         print(f"Resultado: {resultado}")
     else:
         print("Error: No se puede dividir entre cero")
